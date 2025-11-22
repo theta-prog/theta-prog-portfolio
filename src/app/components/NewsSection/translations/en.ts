8514f5c6284d3e8469c0852a81b6895a74f0c9cd
theta-prog <yudengdaotian@gmail.com>
2025-11-22 16:48:32 +0900

    fix

--- src/app/components/NewsSection/translations/en.ts
+++ src/app/components/NewsSection/translations/en.ts
@@ -1,3 +1,9 @@
 export const en = {
     title: 'News',
+    newsItems: [
+        {
+            date: '2024.05.20',
+            content: 'Portfolio Renewal',
+        },
+    ],
 };
--- src/app/components/NewsSection/translations/ja.ts
+++ src/app/components/NewsSection/translations/ja.ts
@@ -1,3 +1,9 @@
 export const ja = {
     title: 'News',
+    newsItems: [
+        {
+            date: '2024.05.20',
+            content: 'Portfolio Renewal',
+        },
+    ],
 };
