3ea8298f56c0c59a887e20853f8157a65131b0d4
theta-prog <yudengdaotian@gmail.com>
2025-11-23 16:02:18 +0900

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: "standalone",
-  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
   typescript: {
     ignoreBuildErrors: true
   }
